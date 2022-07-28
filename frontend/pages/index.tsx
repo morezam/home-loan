d0c22a6b01fc9a19608c65bc3bd2ac440c42d343
Mohammad Hamzehei <mohammadreza.ha.va@gmail.com>
2022-07-28 13:03:54 +0430

    creating an overall folder and a backend folder

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+// import Image from 'next/image'
+// import styles from '../styles/Home.module.css'
+
+import React from 'react';
+
+const WelcomeBack: NextPage = () => {
+	return (
+		<div>
+			<Head>
+				<title>صندوق قرض الحسنه خانگی ونداده</title>
+			</Head>
+		</div>
+	);
+};
+
+export default WelcomeBack;
